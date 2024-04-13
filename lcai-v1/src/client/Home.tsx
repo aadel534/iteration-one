730042733ef1246f45033197b2785445a6a3ac32
aadel534 <adelayo.adebiyi@gmail.com>
2024-04-13 23:54:50 +0100

    Responsive front-end design for homepage

--- lcai-v1/src/client/Home.tsx
+++ lcai-v1/src/client/Home.tsx
@@ -2,25 +2,29 @@ function Home() {
 
   return (
     <>
-    <header>
-      <h1 className="pt-10 text-4xl font-mono font-extrabold	"><span id="lcaiLogoLeft" className="text-rose-700 pr-4">LCAI!</span>LightsCameraAI!</h1>
-      <nav className="bg-white">
-        <ul className="flex space-x-2 float-right mr-10	">
+    <header className="md:flex md:justify-between md:items-center -mt-10 bg-slate-300 pb-10">
+      <h1 className="pt-10 text-4xl font-mono font-extrabold ml-6 mt-6"><span id="lcaiLogoLeft" className="text-rose-700 pr-4">LCAI!</span>LightsCameraAI!</h1>
+      <nav className="mt-10 md:mt-20 ">
+        <ul className="md:flex space-x-2  mr-10	text-center ">
 
-          <li><a href="#" className="text-3xl pr-10">Register</a></li>
-          <li><a href="#" className="text-3xl">Log In</a></li>
+          <li><a href="#" className="text-3xl md:pr-10 uppercase md:capitalize">Register</a></li>
+          <li className="text-slate-300 md:hidden"><a href="#">spacing</a></li>
+          <li><a href="#" className="text-3xl uppercase md:capitalize">Log In</a></li>
         </ul>
       </nav>
     </header>
     <main>
       {/* First commit to Github */}
-      <section className="flex justify-center">
-        <h2>Learn how to act, present, and tell stories using AI!</h2>
-        <figure className="flex justify-center">
-          <img src="actingmask.jpg" alt="actingMask" />
+      <section>
+        <h2 className="ml-6 mt-20 flex justify-center text-3xl text-wrap mb-10">Learn how to act, present, and tell stories using AI!</h2>
+        <figure className="flex justify-center ">
+          <img src="actingmask.jpg" alt="actingMask" className="w-20 md:w-32 lg:w-60" />
         </figure>
-        <button>JOIN NOW!</button>
       </section>
+      <article className="flex justify-center mt-20">
+      <button className="bg-slate-400 text-3xl" >JOIN NOW!</button>
+      </article>
+
     </main>
     </>
   );
