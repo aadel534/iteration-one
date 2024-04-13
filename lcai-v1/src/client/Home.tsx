9feb7476f53de5fa2c90a9c91dd633865bb98ebe
aadel534 <adelayo.adebiyi@gmail.com>
2024-04-13 16:08:16 +0100

    Initial commit

---
+++ lcai-v1/src/client/Home.tsx
@@ -0,0 +1,28 @@
+function Home() {
+
+  return (
+    <>
+    <header>
+      <h1 className="pt-10 text-4xl font-mono font-extrabold	"><span id="lcaiLogoLeft" className="text-rose-700 pr-4">LCAI!</span>LightsCameraAI!</h1>
+      <nav className="bg-white">
+        <ul className="flex space-x-2 float-right mr-10	">
+
+          <li><a href="#" className="text-3xl pr-10">Register</a></li>
+          <li><a href="#" className="text-3xl">Log In</a></li>
+        </ul>
+      </nav>
+    </header>
+    <main>
+      <section className="flex justify-center">
+        <h2>Learn how to act, present, and tell stories using AI!</h2>
+        <figure className="flex justify-center">
+          <img src="actingmask.jpg" alt="actingMask" />
+        </figure>
+        <button>JOIN NOW!</button>
+      </section>
+    </main>
+    </>
+  );
+}
+
+export default Home;
---
+++ lcai-v1/src/client/main.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import 'tailwindcss/tailwind.css';
+import Home from "./Home";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <Home/>
+  </React.StrictMode>,
+);
