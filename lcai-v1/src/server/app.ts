9436b4d84f15c1080dc216685fc0ca7aa23594f7
aadel534 <56454954+aadel534@users.noreply.github.com>
2024-05-03 21:12:15 +0100

    Enable use of routes

--- lcai-v1/src/server/app.ts
+++ lcai-v1/src/server/app.ts
@@ -2,12 +2,11 @@
 import express from 'express';
 import cors from 'cors';
 import path from 'path';
+import { routePaths } from './routes/routes.ts'; 
 import { fileURLToPath } from 'url';
-
 const app = express();
 app.use(cors());
-app.use(express.json());
-
+app.use(routePaths);
 
 // Dynamically generate paths
 const __filename = fileURLToPath(import.meta.url);
--- lcai-v1/src/server/routes/routes.ts
+++ lcai-v1/src/server/routes/routes.ts
@@ -2,14 +2,16 @@ import {registerUser, loginUser} from '../controllers/userController.js';
 import app from "../app.js";
 import path from "path";
 
-app.post("/api/login", (req, res) => {
-  console.log("Login route hit");
-  loginUser(req, res);
-}); 
-app.post("/api/register", registerUser)
+export function routePaths() {
+
+app.post("/api/login", loginUser);
+
+app.post("/api/register", registerUser);
 
 // Single page application with single entry point (routes handled by client-side), redirects to home page
 // when user makes a get request to '/register' for example 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', '..','..', 'dist', 'index.html'));
 });
+
+}
