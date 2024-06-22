6a316a0cefe78301314de0e6d291cd41d7d6ae18
aadel534 <56454954+aadel534@users.noreply.github.com>
2024-06-22 09:34:05 +0100

    Add comments to facerecog.py

--- lcai-v1/public/ai_models/face_recog/facerecog.py
+++ lcai-v1/public/ai_models/face_recog/facerecog.py
@@ -1,25 +1,22 @@
+# Import necessary libraries
+import glob as glob  # For file pattern matching
+import os  # For interacting with the operating system
+import tensorflow as tf  # For deep learning tasks
+from keras import layers, models  # For building neural network models
+from pathlib import Path  # For handling file paths
+import time  # For time-related functions
+import keras  # High-level neural networks API
 
-#Search for files (pattern matching) using glob
-import glob as glob
-import os
-import tensorflow as tf
-from keras import layers, models
-from pathlib import Path
-import time
-import keras
-
-
-# MY DATASET
+# training and testing datasets
 path_test = "/Users/adelayoadebiyi/Documents/GitHub/iteration-one/lcai-v1/public/datasets/emotions/test"
 path_train = "/Users/adelayoadebiyi/Documents/GitHub/iteration-one/lcai-v1/public/datasets/emotions/train"
 
-
-
-#Source: https://martinxpn.medium.com/pathlib-the-oop-approach-of-working-with-file-system-in-python-65-100-days-of-python-7dbf85e9cec2#:~:text=Creating%20Path%20Objects%20With%20pathlib,based%20on%20the%20operating%20system.
+# Creating Path objects for training and testing datasets
+# Source: https://martinxpn.medium.com/pathlib-the-oop-approach-of-working-with-file-system-in-python-65-100-days-of-python-7dbf85e9cec2#:~:text=Creating%20Path%20Objects%20With%20pathlib,based%20on%20the%20operating%20system.
 data_dir = Path(path_train) 
 test_data_dir = Path(path_test)
-# Set consistent
-#  image sizes and number of images in each training batch
+
+# Set image size and batch size for training
 batch_size = 64
 img_height = 48
 img_width = 48
@@ -29,15 +26,15 @@ img_width = 48
 AUTOTUNE = tf.data.AUTOTUNE
 
 # Keep image size consistent
-## Source: https://www.tensorflow.org/tutorials/load_data/images
+# Source: https://www.tensorflow.org/tutorials/load_data/images
 IMG_SIZE = 48
 resize_and_rescale = tf.keras.Sequential([
   layers.Resizing(IMG_SIZE, IMG_SIZE),
   layers.Rescaling(1./255)
 ])
 
-
-# Source https://www.tensorflow.org/tutorials/load_data/images
+# Load training dataset with image dataset utility
+# Source: https://www.tensorflow.org/tutorials/load_data/images
 train_ds = tf.keras.utils.image_dataset_from_directory(
   data_dir,
   validation_split=0.2,
@@ -46,8 +43,9 @@ train_ds = tf.keras.utils.image_dataset_from_directory(
   image_size=(img_height, img_width),
   batch_size=batch_size,
   color_mode="grayscale"
-  )
+)
 
+# Load validation dataset with image dataset utility
 val_ds = tf.keras.utils.image_dataset_from_directory(
   data_dir,
   validation_split=0.2,
@@ -55,74 +53,63 @@ val_ds = tf.keras.utils.image_dataset_from_directory(
   seed=123,
   image_size=(img_height, img_width),
   batch_size=batch_size,
-  color_mode="grayscale")
+  color_mode="grayscale"
+)
 
-
+# Load testing dataset with image dataset utility
 test_ds = tf.keras.utils.image_dataset_from_directory(
     test_data_dir,
-    shuffle=True,        
+    shuffle=True,
     batch_size=batch_size,
-    image_size=(img_height, img_width,),
-    color_mode="grayscale")
+    image_size=(img_height, img_width),
+    color_mode="grayscale"
+)
 
-
-
-
+# Retrieve class names from the training dataset
 class_names = train_ds.class_names
 print(class_names)
-# print(len(train_ds))
 
-
+# Normalize pixel values to the range [0, 1]
 # Source: https://www.tensorflow.org/tutorials/load_data/images
-# RGB channel values are in the [0, 255] range, which isn't ideal for a neural network. 
-# Make input values small.
 normalization_layer = tf.keras.layers.Rescaling(1./255)
 normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
 
-# Improve performance when using images 
+# Improve performance by caching and prefetching data
 # Source: https://www.tensorflow.org/tutorials/load_data/images
 train_ds = train_ds.cache().prefetch(buffer_size=AUTOTUNE)
 val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
 
-
-# Data augmentation - atempt later
+# Data augmentation to improve model robustness
 data_augmentation = tf.keras.Sequential([
     layers.RandomRotation(0.1),
     layers.RandomContrast(0.1),
     layers.RandomBrightness(factor=0.1)
 ])
 
-
-
-# and
+# Function to prepare datasets with optional shuffling and augmentation
 def prepare(ds, shuffle=False, augment=False):
-  # Resize and rescale all datasets.
+  # Resize and rescale all datasets
   ds = ds.map(lambda x, y: (resize_and_rescale(x), y), 
               num_parallel_calls=AUTOTUNE)
   
-  # Data augmentation
+  # Apply data augmentation if specified
   if augment:
       ds = ds.map(lambda x, y: (data_augmentation(x, training=True), y),
                   num_parallel_calls=AUTOTUNE)
 
-
+  # Shuffle the dataset if specified
   if shuffle:
     ds = ds.shuffle(1000)
 
-  # Use buffered prefetching on all datasets.
+  # Use buffered prefetching on all datasets
   return ds.prefetch(buffer_size=AUTOTUNE)
 
-
-
-# Prepare and preprocess data
+# Prepare and preprocess datasets
 train_ds = prepare(train_ds, shuffle=True, augment=False) 
 val_ds = prepare(val_ds)
 test_ds = prepare(test_ds)
 
-
-
-# ***** MY MODEL *****
-
+# Define the neural network model
 model = tf.keras.Sequential([
     layers.Conv2D(16, 3, padding='same', activation='elu'),
     layers.MaxPooling2D((2, 2)),
@@ -135,28 +122,31 @@ model = tf.keras.Sequential([
     layers.Dense(64, activation='relu'),
     layers.Dense(7)
 ])
+
+# Compile the model with optimizer and loss function
 model.compile(optimizer='RMSprop',
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
-epochs = 1000
 
+# Train the model for a specified number of epochs
+epochs = 1000
 history = model.fit(
   train_ds,
   validation_data=val_ds,
   epochs=epochs
 )
 
-
+# Evaluate the model on the test dataset
 loss, acc = model.evaluate(test_ds)
-print("Accuracy", acc)
-# Metrics explained
+print("Test Accuracy", acc)
+print("Test Loss:", loss)
+
+
+# Explanation of metrics
 # Accuracy is the total accuracy for all the predictions made so far as a percentage
-#Loss is the difference between the prediiction and actual value (for every instance that the algortihm has run so far) 
-# as a percentage 
-# Val_Loss is the difference between the prediction and the actual value for each instance of unseen data
-# going through the algorithm during training (forr )
-# Val_accuracy is the accuracy per instance 
+# Loss is the difference between the prediction and actual value (for every instance that the algorithm has run so far) as a percentage
+# Val_Loss is the difference between the prediction and the actual value for each instance of unseen data going through the algorithm during training
+# Val_Accuracy is the accuracy per instance
 
-
-
+# Save the trained model
 keras.saving.save_model(model, 'originalemotionrecogniser.keras')
